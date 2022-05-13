44b640588528ad939fd68a787bbc32218679442f
AkhdanFirdaus <akhdan.musyaffa.firdaus@gmail.com>
2022-05-13 18:44:16 +0700

    [ADD] Firebase Injection

--- lib/injection.config.dart
+++ lib/injection.config.dart
@@ -4,14 +4,25 @@
 // InjectableConfigGenerator
 // **************************************************************************
 
+import 'package:firebase_auth/firebase_auth.dart' as _i3;
 import 'package:get_it/get_it.dart' as _i1;
-import 'package:injectable/injectable.dart'
-    as _i2; // ignore_for_file: unnecessary_lambdas
+import 'package:google_sign_in/google_sign_in.dart' as _i4;
+import 'package:injectable/injectable.dart' as _i2;
+
+import 'infrastructure/core/firebase_injection_module.dart'
+    as _i5; // ignore_for_file: unnecessary_lambdas
 
 // ignore_for_file: lines_longer_than_80_chars
 /// initializes the registration of provided dependencies inside of [GetIt]
 _i1.GetIt $initGetIt(_i1.GetIt get,
     {String? environment, _i2.EnvironmentFilter? environmentFilter}) {
   final gh = _i2.GetItHelper(get, environment, environmentFilter);
+  final firebaseInjectableModule = _$FirebaseInjectableModule();
+  gh.lazySingleton<_i3.FirebaseAuth>(
+      () => firebaseInjectableModule.firebaseAuth);
+  gh.lazySingleton<_i4.GoogleSignIn>(
+      () => firebaseInjectableModule.googleSignIn);
   return get;
 }
+
+class _$FirebaseInjectableModule extends _i5.FirebaseInjectableModule {}
