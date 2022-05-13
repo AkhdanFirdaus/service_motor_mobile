d787525f3d817beb8254a6bc5e33c5b98718967a
AkhdanFirdaus <akhdan.musyaffa.firdaus@gmail.com>
2022-05-13 19:48:28 +0700

    [ADD] Firestore and auth domain

---
+++ lib/domain/auth/i_auth_facade.dart
@@ -0,0 +1,16 @@
+import 'package:dartz/dartz.dart';
+import 'package:service_motor_mobile/domain/auth/auth_failure.dart';
+import 'package:service_motor_mobile/domain/core/value_objects.dart';
+
+abstract class IAuthFacade {
+  Future<Either<AuthFailure, Unit>> registerWithEmailPassword({
+    required EmailAddress emailAddress,
+    required Password password,
+  });
+  Future<Either<AuthFailure, Unit>> signInWithEmailPassword({
+    required EmailAddress emailAddress,
+    required Password password,
+  });
+  Future<Either<AuthFailure, Unit>> signInWithGoogle();
+  Future<void> signOut();
+}
--- lib/injection.config.dart
+++ lib/injection.config.dart
@@ -4,13 +4,14 @@
 // InjectableConfigGenerator
 // **************************************************************************
 
+import 'package:cloud_firestore/cloud_firestore.dart' as _i4;
 import 'package:firebase_auth/firebase_auth.dart' as _i3;
 import 'package:get_it/get_it.dart' as _i1;
-import 'package:google_sign_in/google_sign_in.dart' as _i4;
+import 'package:google_sign_in/google_sign_in.dart' as _i5;
 import 'package:injectable/injectable.dart' as _i2;
 
 import 'infrastructure/core/firebase_injection_module.dart'
-    as _i5; // ignore_for_file: unnecessary_lambdas
+    as _i6; // ignore_for_file: unnecessary_lambdas
 
 // ignore_for_file: lines_longer_than_80_chars
 /// initializes the registration of provided dependencies inside of [GetIt]
@@ -20,9 +21,11 @@ _i1.GetIt $initGetIt(_i1.GetIt get,
   final firebaseInjectableModule = _$FirebaseInjectableModule();
   gh.lazySingleton<_i3.FirebaseAuth>(
       () => firebaseInjectableModule.firebaseAuth);
-  gh.lazySingleton<_i4.GoogleSignIn>(
+  gh.lazySingleton<_i4.FirebaseFirestore>(
+      () => firebaseInjectableModule.firestore);
+  gh.lazySingleton<_i5.GoogleSignIn>(
       () => firebaseInjectableModule.googleSignIn);
   return get;
 }
 
-class _$FirebaseInjectableModule extends _i5.FirebaseInjectableModule {}
+class _$FirebaseInjectableModule extends _i6.FirebaseInjectableModule {}
