c6c21947748d35b355706a4e09b5090840c0611e
AkhdanFirdaus <akhdan.musyaffa.firdaus@gmail.com>
2022-05-14 14:19:13 +0700

    [SETUP] Firebase using Firebase CLI

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:injectable/injectable.dart';
+import 'package:service_motor_mobile/firebase_options.dart';
 import 'package:service_motor_mobile/injection.dart';
 import 'package:service_motor_mobile/presentation/core/app_widget.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   configureInjection(Environment.prod);
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(AppWidget());
 }
