d30d51c479ad05924684a0f6de9f1a0322a20bbd
AkhdanFirdaus <akhdan.musyaffa.firdaus@gmail.com>
2022-05-17 22:29:34 +0700

    [ADD] Gear background logo

--- lib/presentation/ticket/ticket_detail/ticket_detail_page.dart
+++ lib/presentation/ticket/ticket_detail/ticket_detail_page.dart
@@ -10,74 +10,83 @@ class TicketDetailPage extends StatelessWidget {
     AutoRouter.of(context);
     return Scaffold(
       body: SafeArea(
-        child: ListView(
-          padding: const EdgeInsets.all(30),
+        child: Stack(
           children: [
-            const Chip(
-              backgroundColor: AppColor.orange,
-              label: Text(
-                '#NP123JD',
-                style: TextStyle(color: AppColor.white),
-                textAlign: TextAlign.center,
-              ),
+            Positioned(
+              bottom: -100,
+              right: -60,
+              child: Image.asset('assets/other/gear.png'),
             ),
-            const SizedBox(height: 16),
-            const Text(
-              'AHASS Soekarno Hatta',
-              style: AppFont.headline1,
-              textAlign: TextAlign.center,
-            ),
-            Text(
-              '10 Mei 2022',
-              style: AppFont.subhead2.copyWith(color: AppColor.greyOrange),
-              textAlign: TextAlign.center,
-            ),
-            const SizedBox(height: 30),
-            const Center(
-              child: SizedBox(
-                width: 200,
-                height: 200,
-                child: Placeholder(),
-              ),
-            ),
-            const SizedBox(height: 30),
-            FractionallySizedBox(
-              widthFactor: 0.5,
-              child: ElevatedButton(
-                onPressed: () {},
-                child: const Text('Unduh Tiket'),
-              ),
-            ),
-            const SizedBox(height: 30),
-            const Text(
-              'Catatan',
-              style: AppFont.subhead2,
-              textAlign: TextAlign.left,
-            ),
-            const SizedBox(height: 8),
-            const Text(
-              'Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.',
-              textAlign: TextAlign.justify,
-            ),
-            const SizedBox(height: 30),
-            FractionallySizedBox(
-              widthFactor: 1,
-              child: ElevatedButton(
-                onPressed: () {
-                  context.router.push(const TicketChatRoute());
-                },
-                child: const Text('Hubungi Admin'),
-              ),
-            ),
-            const SizedBox(height: 16),
-            FractionallySizedBox(
-              widthFactor: 1,
-              child: OutlinedButton(
-                onPressed: () {
-                  context.router.pop();
-                },
-                child: const Text('Kembali ke Menu Utama'),
-              ),
+            ListView(
+              padding: const EdgeInsets.all(30),
+              children: [
+                const Chip(
+                  backgroundColor: AppColor.orange,
+                  label: Text(
+                    '#NP123JD',
+                    style: TextStyle(color: AppColor.white),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                const SizedBox(height: 16),
+                const Text(
+                  'AHASS Soekarno Hatta',
+                  style: AppFont.headline1,
+                  textAlign: TextAlign.center,
+                ),
+                Text(
+                  '10 Mei 2022',
+                  style: AppFont.subhead2.copyWith(color: AppColor.greyOrange),
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(height: 30),
+                const Center(
+                  child: SizedBox(
+                    width: 200,
+                    height: 200,
+                    child: Placeholder(),
+                  ),
+                ),
+                const SizedBox(height: 30),
+                FractionallySizedBox(
+                  widthFactor: 0.5,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: const Text('Unduh Tiket'),
+                  ),
+                ),
+                const SizedBox(height: 30),
+                const Text(
+                  'Catatan',
+                  style: AppFont.subhead2,
+                  textAlign: TextAlign.left,
+                ),
+                const SizedBox(height: 8),
+                const Text(
+                  'Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.',
+                  textAlign: TextAlign.justify,
+                ),
+                const SizedBox(height: 30),
+                FractionallySizedBox(
+                  widthFactor: 1,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      context.router.push(const TicketChatRoute());
+                    },
+                    child: const Text('Hubungi Admin'),
+                  ),
+                ),
+                const SizedBox(height: 16),
+                FractionallySizedBox(
+                  widthFactor: 1,
+                  child: OutlinedButton(
+                    onPressed: () {
+                      context.router.pop();
+                    },
+                    child: const Text('Kembali ke Menu Utama'),
+                  ),
+                ),
+              ],
             ),
           ],
         ),
--- lib/presentation/ticket/ticket_detail/ticket_success_page.dart
+++ lib/presentation/ticket/ticket_detail/ticket_success_page.dart
@@ -9,56 +9,66 @@ class TicketSuccessPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Container(
-          padding: const EdgeInsets.all(30),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text(
-                'Pesan Tiket Berhasil',
-                style: AppFont.headline3,
-                textAlign: TextAlign.center,
+        child: Stack(
+          children: [
+            Positioned(
+              bottom: -100,
+              right: -60,
+              child: Image.asset('assets/other/gear.png'),
+            ),
+            Container(
+              padding: const EdgeInsets.all(30),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    'Pesan Tiket Berhasil',
+                    style: AppFont.headline3,
+                    textAlign: TextAlign.center,
+                  ),
+                  const SizedBox(height: 8),
+                  Text(
+                    'Jangan lupa datang tepat waktu, ya!',
+                    style:
+                        AppFont.subhead3.copyWith(color: AppColor.greyOrange),
+                    textAlign: TextAlign.center,
+                  ),
+                  const SizedBox(height: 30),
+                  const Chip(
+                    backgroundColor: AppColor.orange,
+                    label: Text(
+                      'Kode Tiket: 18239172389',
+                      style: TextStyle(color: AppColor.white),
+                    ),
+                  ),
+                  const SizedBox(height: 16),
+                  const SizedBox(
+                    width: 200,
+                    height: 200,
+                    child: Placeholder(),
+                  ),
+                  const SizedBox(height: 30),
+                  FractionallySizedBox(
+                    widthFactor: 1,
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      child: const Text('Unduh Tiket'),
+                    ),
+                  ),
+                  const SizedBox(height: 16),
+                  FractionallySizedBox(
+                    widthFactor: 1,
+                    child: OutlinedButton(
+                      onPressed: () {
+                        context.router.pop();
+                      },
+                      child: const Text('Kembali ke Menu Utama'),
+                    ),
+                  ),
+                ],
               ),
-              const SizedBox(height: 8),
-              Text(
-                'Jangan lupa datang tepat waktu, ya!',
-                style: AppFont.subhead3.copyWith(color: AppColor.greyOrange),
-                textAlign: TextAlign.center,
-              ),
-              const SizedBox(height: 30),
-              const Chip(
-                backgroundColor: AppColor.orange,
-                label: Text(
-                  'Kode Tiket: 18239172389',
-                  style: TextStyle(color: AppColor.white),
-                ),
-              ),
-              const SizedBox(height: 16),
-              const SizedBox(
-                width: 200,
-                height: 200,
-                child: Placeholder(),
-              ),
-              const SizedBox(height: 30),
-              FractionallySizedBox(
-                widthFactor: 1,
-                child: ElevatedButton(
-                  onPressed: () {},
-                  child: const Text('Unduh Tiket'),
-                ),
-              ),
-              const SizedBox(height: 16),
-              FractionallySizedBox(
-                widthFactor: 1,
-                child: OutlinedButton(
-                  onPressed: () {
-                    context.router.pop();
-                  },
-                  child: const Text('Kembali ke Menu Utama'),
-                ),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
